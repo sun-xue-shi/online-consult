7a5b02268892332cd57b5e2da63aacf22cd376d3
sun-xue-shi <2595531421@qq.com>
2023-12-06 00:04:34 +0800

    feat:请求函数封装

---
+++ src/utils/request.ts
@@ -0,0 +1,39 @@
+import router from '@/router'
+import { useUserStore } from '@/stores'
+import axios from 'axios'
+import { Toast } from 'vant'
+
+const baseURL = ''
+const request = axios.create({
+  baseURL,
+  timeout: 10000
+})
+
+const userStore = useUserStore()
+request.interceptors.request.use(
+  (config) => {
+    if (userStore.user.token && config.headers) {
+      config.headers['Authorization'] = `Bearer ${userStore.user.token}`
+    }
+
+    return config
+  },
+  (err) => Promise.reject(err)
+)
+
+request.interceptors.response.use(
+  (res) => {
+    if (res.data?.code !== 10000) {
+      new Toast(res.data?.message)
+      return Promise.reject(res.data)
+    }
+    return res.data
+  },
+  (err) => {
+    if (err.response.status === 401) {
+      userStore.removeUser()
+      router.push(`/login?returnUrl=${router.currentRoute.value.fullPath}`)
+    }
+    return Promise.reject(err)
+  }
+)
