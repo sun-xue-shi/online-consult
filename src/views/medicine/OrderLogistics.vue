690377cfc799ec64f8aba2fade14dd7bc606e205
sun-xue-shi <2595531421@qq.com>
2024-01-08 17:21:22 +0800

    feat：药品订单物流信息

---
+++ src/views/medicine/OrderLogistics.vue
@@ -0,0 +1,124 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="order-logistics-page">
+    <div id="map">
+      <div class="title">
+        <van-icon name="arrow-left" @click="$router.back()" />
+        <span></span>
+        <van-icon name="service" />
+      </div>
+      <div class="current">
+        <p class="status">订单派送中 预计1tian送达</p>
+        <p class="predict">
+          <span></span>
+          <span></span>
+        </p>
+      </div>
+    </div>
+    <div class="logistics">
+      <p class="title">物流详情</p>
+      <van-steps direction="vertical" :active="0">
+        <van-step v-for="item in 3" :key="item">
+          <p class="status"></p>
+          <p class="content"></p>
+          <p class="time"></p>
+        </van-step>
+      </van-steps>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.order-logistics-page {
+  --van-step-icon-size: 18px;
+  --van-step-circle-size: 10px;
+}
+#map {
+  height: 450px;
+  background-color: var(--bg);
+  overflow: hidden;
+  position: relative;
+  .title {
+    background-color: #fff;
+    height: 46px;
+    width: 355px;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    padding: 0 15px;
+    font-size: 16px;
+    position: absolute;
+    left: 10px;
+    top: 10px;
+    box-sizing: border-box;
+    box-shadow: 0px 0px 22px 0px rgba(229, 229, 229, 0.5);
+    z-index: 999;
+    > span {
+      flex: 1;
+      text-align: center;
+    }
+    .van-icon {
+      font-size: 18px;
+      color: #666;
+      &:last-child {
+        color: var(--primary);
+      }
+    }
+  }
+  .current {
+    height: 80px;
+    border-radius: 4px;
+    background-color: #fff;
+    height: 80px;
+    width: 355px;
+    box-sizing: border-box;
+    padding: 15px;
+    position: absolute;
+    left: 10px;
+    bottom: 10px;
+    box-shadow: 0px 0px 22px 0px rgba(229, 229, 229, 0.5);
+    z-index: 999;
+    .status {
+      font-size: 15px;
+      line-height: 26px;
+    }
+    .predict {
+      color: var(--tip);
+      font-size: 13px;
+      margin-top: 5px;
+      > span {
+        padding-right: 10px;
+      }
+    }
+  }
+}
+.logistics {
+  padding: 0 10px 20px;
+  .title {
+    font-size: 16px;
+    padding: 15px 5px 5px;
+  }
+  .van-steps {
+    :deep(.van-step) {
+      &::after {
+        display: none;
+      }
+    }
+    .status {
+      font-size: 15px;
+      color: var(--text3);
+      margin-bottom: 4px;
+    }
+    .content {
+      font-size: 13px;
+      color: var(--tip);
+      margin-bottom: 4px;
+    }
+    .time {
+      font-size: 13px;
+      color: var(--tag);
+    }
+  }
+}
+</style>
